46cc8cedba7e60fe86731444150b90dcd5952ca8
massambadiop-unchk <massamba.diop3@unchk.edu.sn>
2026-07-10 00:08:03 +0000

    Add files via upload

---
+++ models/senshop.sql
@@ -0,0 +1,235 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : ven. 10 juil. 2026 à 02:07
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `senshop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categorie`
+--
+
+CREATE TABLE `categorie` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `descript` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `categorie`
+--
+
+INSERT INTO `categorie` (`id`, `nom`, `descript`) VALUES
+(14, 'Chaussures', ''),
+(15, 'Sacs', ''),
+(18, 'Electroniques', '');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commande`
+--
+
+CREATE TABLE `commande` (
+  `id` int(11) NOT NULL,
+  `produit_id` int(11) NOT NULL,
+  `quantite` int(11) NOT NULL DEFAULT 1,
+  `prenom` varchar(100) NOT NULL,
+  `nom` varchar(100) NOT NULL,
+  `telephone` varchar(20) NOT NULL,
+  `adresse` varchar(255) NOT NULL,
+  `ville` varchar(100) DEFAULT NULL,
+  `region` varchar(100) DEFAULT NULL,
+  `repere` text DEFAULT NULL,
+  `date_commande` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `commande`
+--
+
+INSERT INTO `commande` (`id`, `produit_id`, `quantite`, `prenom`, `nom`, `telephone`, `adresse`, `ville`, `region`, `repere`, `date_commande`) VALUES
+(16, 22, 1, 'Massamba', 'Diop', '+221781238766', 'GRAND MEDINE ILOSY N766', 'Dakar', 'Dakar', 'Ecole teranga', '2026-07-09 10:56:36'),
+(17, 22, 2, 'Massamba', 'Diop', '+221781238766', 'GRAND MEDINE ILOSY N766', 'Dakar', 'Dakar', 'ecole PA', '2026-07-09 11:03:55');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contact`
+--
+
+CREATE TABLE `contact` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `telephone` varchar(20) NOT NULL,
+  `sujet` varchar(100) NOT NULL,
+  `message` text NOT NULL,
+  `date_envoi` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `contact`
+--
+
+INSERT INTO `contact` (`id`, `nom`, `email`, `telephone`, `sujet`, `message`, `date_envoi`) VALUES
+(5, 'Massamba Diop', 'massd432@gmail.com', '781238766', 'Commande', 'J\'ai pas encore reçu ma commande', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produits`
+--
+
+CREATE TABLE `produits` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `categorie` varchar(30) NOT NULL,
+  `prix` int(11) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `descript` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `produits`
+--
+
+INSERT INTO `produits` (`id`, `nom`, `categorie`, `prix`, `stock`, `descript`, `image`) VALUES
+(22, 'Sac à main', '15', 35, 8, 'Sac Premium', '6a36cb9c3b3cb.png'),
+(23, 'Casque sans fil', '16', 12, 5, 'Casque sans fil', '6a36cc73585f2.png'),
+(24, 'Sac pour femme ', '15', 12000, 13, 'sac élégante nfngjgjrj', '6a4184ac9af3f.png'),
+(25, 'Sac à main', '15', 12345, 12, '', '6a4f814398442.png');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `nom` varchar(30) NOT NULL,
+  `tel` varchar(20) NOT NULL,
+  `adresse` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `mdp` varchar(255) NOT NULL,
+  `role` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `prenom`, `nom`, `tel`, `adresse`, `email`, `mdp`, `role`) VALUES
+(6, 'Doudou', 'Fall', '7812435678', 'Dakar', 'Doudou@gmail.com', 'Doudou221', 'admin'),
+(7, 'assane', 'faye', '778678986', 'Grand medine', 'Doudou@gmail.com', 'Doudou221', 'client'),
+(8, 'assane', 'faye', '7812435678', 'Patte d\'oie', 'assane@gmail.com', 'assane221', 'client'),
+(9, 'Massamba', 'joop', '77454678', 'Grand M', 'mass@gmail.com', 'mass221', 'client'),
+(10, 'Massamba', 'Diop', '7812348766', 'GRAND MEDINE ILOSY N766', 'maker@gmail.com', 'Massamba2000', 'client'),
+(11, 'Sarabia', 'ndiath', '774356768', 'Patte d\'oie', 'etudiant-unchk@gmail.com', 'Massamba2000', 'client'),
+(12, 'Massamba', 'Diop', '781238766', 'Patte d\'oie', 'mass@gmail.com', 'Massamba2000@', 'client'),
+(13, 'Massamba', 'Diop', '781243565', 'GRAND MEDINE ILOSY N766', 'ahmad@gmail.com', 'Massamba2000@', 'client');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `categorie`
+--
+ALTER TABLE `categorie`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `commande`
+--
+ALTER TABLE `commande`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `produit_id` (`produit_id`);
+
+--
+-- Index pour la table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `produits`
+--
+ALTER TABLE `produits`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `categorie`
+--
+ALTER TABLE `categorie`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT pour la table `commande`
+--
+ALTER TABLE `commande`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT pour la table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `produits`
+--
+ALTER TABLE `produits`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `commande`
+--
+ALTER TABLE `commande`
+  ADD CONSTRAINT `commande_ibfk_1` FOREIGN KEY (`produit_id`) REFERENCES `produits` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
